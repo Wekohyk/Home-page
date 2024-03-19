3ae67bcdbeadaece4fef81cc7ebb4468978eda29
huangyukai <wekohyk@gmail.com>
2024-03-19 16:57:24 +0800

    Remove @unocss/eslint-config dependency and add background

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
-  <div>App router</div>
+  <!-- background -->
+  <div class="fixed left-0 top-0 w-100% h-100%">
+    <Background></Background>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Background from './components/Background.vue';
+</script>
 
 <style scoped lang="scss"></style>
