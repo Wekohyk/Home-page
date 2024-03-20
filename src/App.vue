4b9ab41fedf2a5e90134d8dcd2edffb2d7cbfc68
huangyukai <wekohyk@gmail.com>
2024-03-20 14:31:50 +0800

    Refactor loading animation in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <template>
   <div class="flex justify-center items-center">
-    <div
-      class="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-100vw h-100vh bg-#000 flex justify-center items-center flex-col"
+    <transition
+      enter-from-class="opacity-0"
+      enter-to-class="opacity-100"
+      leave-from-class="opacity-100"
+      leave-to-class="opacity-0"
     >
-      <Loading></Loading>
-    </div>
+      <div
+        v-if="loading"
+        class="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-100vw h-100vh bg-#000 flex justify-center items-center flex-col transition-opacity duration-800 ease-in"
+      >
+        <Loading></Loading>
+      </div>
+    </transition>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 import Loading from './components/Loading.vue';
+
+// loading
+const loading = ref(true);
+setTimeout(() => {
+  loading.value = false;
+}, 2000);
 </script>
 
 <style scoped lang="scss"></style>
