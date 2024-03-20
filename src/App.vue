b66f1bdc66601b02b152fe04c1f3a338db736e02
huangyukai <wekohyk@gmail.com>
2024-03-20 14:49:05 +0800

    Add backgroundImgMask and remove background component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="flex justify-center items-center">
+    <!-- loading animation -->
     <transition
       enter-from-class="opacity-0"
       enter-to-class="opacity-100"
@@ -8,11 +9,14 @@
     >
       <div
         v-if="loading"
-        class="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-100vw h-100vh bg-#000 flex justify-center items-center flex-col transition-opacity duration-800 ease-in"
+        class="fixed z-999 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-100vw h-100vh bg-#000 flex justify-center items-center flex-col transition-opacity duration-800 ease-in"
       >
         <Loading></Loading>
       </div>
     </transition>
+
+    <!-- backgroundImg -->
+    <div class="fixed left-0 top-0 w-100vw h-100vh opacity-70 bg-#000"></div>
   </div>
 </template>
 
