090e638cab04c2434372966c9d5b2403cddb55ce
huangyukai <wekohyk@gmail.com>
2024-06-14 17:27:45 +0800

    refactor: update wallpaper type in backgroundImg store

--- src/stores/modules/backgroundImg.ts
+++ src/stores/modules/backgroundImg.ts
@@ -4,7 +4,7 @@ import { ref } from 'vue';
 export const backgroundImg = defineStore(
   'backgroundImg',
   () => {
-    const coverType = ref<string>('1'); // Wallpaper type
+    const coverType = ref<string>('0'); // Wallpaper type
     const imgLoadStatus = ref<boolean>(false); // Whether the image is loaded
 
     const setImgLoadStatus = (value: boolean) => {
